fc3604c2f3d0233dd9e58d86d7b698fce414c3a8
Maddy <mmcintosh2k14@gmail.com>
2019-12-16 15:34:21 -0500

    creates roles and users migrations

---
+++ database/migrations/20191216152927_users_table.js
@@ -0,0 +1,24 @@
+
+exports.up = function(knex) {
+  return knex.schema.createTable('users', tbl => {
+      tbl.increments();
+
+      tbl
+        .string('username', 128)
+        .notNullable()
+        .unique();
+
+      tbl
+        .integer('role_id')
+        .unsigned()
+        .notNullable()
+        .references('id')
+        .inTable('roles')
+        .onUpdate('CASCADE')
+        .onDelete('RESTRICT');
+  });
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTableIfExists('users');
+};
