1e2d4934b8aa209db8c902f72319e625a3198bee
Maddy <mmcintosh2k14@gmail.com>
2019-12-17 16:57:05 -0500

    adds restricted/ for rescricted paths

--- api/server.js
+++ api/server.js
@@ -40,7 +40,7 @@ server.use(cors());
 server.use(sessions(sessionConfig)); // will add a req.session object
 
 server.use('/api/auth', authRouter);
-server.use('/api/users', usersRouter);
+server.use('/api/restricted/users', usersRouter);
 
 server.get('/', (req, res) => {
 	res.json({ api: "It's working, it's working!!" });
