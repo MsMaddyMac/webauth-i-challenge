964048a6112fb334b4c864fd022018f91a95193d
Maddy <mmcintosh2k14@gmail.com>
2019-12-17 16:25:35 -0500

    restructures files

--- api/server.js
+++ api/server.js
@@ -1,12 +1,27 @@
 const express = require('express');
+const helmet = require('helmet');
+const cors = require('cors');
+const sessions = require('express-session');
+const KnexSessionStore = require('connect-session-knex')(sessions);
 
-const apiRouter = require('./api-router');
-const configureMiddleware = require('./configure-middleware');
+const authRouter = require('../auth/auth-router');
+const usersRouter = require('../users/users-router');
+const knex = require('../database/dbConfig');
 
 const server = express();
 
-configureMiddleware(server);
+const sessionConfig = {};
 
-server.use('/api', apiRouter);
+server.use(helmet());
+server.use(express.json());
+server.use(cors());
+server.use(sessions(sessionConfig)); // will add a req.session object
+
+server.use('/api/auth', authRouter);
+server.use('/api/users', usersRouter);
+
+server.get('/', (req, res) => {
+	res.json({ api: "It's working, it's working!!" });
+});
 
 module.exports = server;
