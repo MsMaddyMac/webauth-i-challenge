9e0f2b551297b2eb85b6690d5e0db9de148a0cc9
Maddy <mmcintosh2k14@gmail.com>
2019-12-17 16:07:22 -0500

    installs cors and connect-session-knex dependencies

--- validators/validators.js
+++ validators/validators.js
@@ -18,7 +18,7 @@ function validateLogin(req, res, next) {
                 if (user && bcrypt.compareSync(password, user.password)) {
                     next();
                 } else {
-                    res.status(401).json({ message: 'Invalid Credentials' });
+                    res.status(401).json({ message: 'You shall not pass!' });
                 }
             })
             .catch(err => {
