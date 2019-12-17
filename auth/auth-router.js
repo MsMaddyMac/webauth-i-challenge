d222fcf9e583a9ac2a4d923f398555633fd8c49a
Maddy <mmcintosh2k14@gmail.com>
2019-12-17 16:49:02 -0500

    creates logout endpoint

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -43,4 +43,16 @@ router.post('/login', (req, res) => {
 		});
 });
 
+router.get('/logout', (req, res) => {
+	if (req.session) {
+		req.session.destroy(err => {
+			if (err) {
+				res.status(500).json({ error: 'Error logging out.' });
+			} else {
+				res.status(200).json({ message: 'Log out successful!' });
+			}
+		});
+	}
+});
+
 module.exports = router;
